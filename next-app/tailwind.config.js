517d48c06e9541e207745aa9f5cc33e38ffcd921
Mathijsvanginneken <mathijsvginneken@gmail.com>
2021-12-14 21:23:14 +0100

    styling ready

--- next-app/tailwind.config.js
+++ next-app/tailwind.config.js
@@ -3,8 +3,21 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
+  theme: {  
     extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
   },
+},
   plugins: [],
 }
